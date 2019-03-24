5f3cf6930d341c427a498fd2e4dd8efeadfe4a62
andre <andre@DESKTOP-D8N4KMU.station>
2019-03-24 11:08:18 +0100

    test

--- src/main/java/com/andrea/taskmanager/TaskmanagerApplication.java
+++ src/main/java/com/andrea/taskmanager/TaskmanagerApplication.java
@@ -7,5 +7,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TaskmanagerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TaskmanagerApplication.class, args);
-	}
+	} 
 }
